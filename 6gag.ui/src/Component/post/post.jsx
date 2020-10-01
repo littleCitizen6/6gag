ce9d6604068ae18fd93869c70b8a31e1acda7da6
littlecitizen6 <thelittlecitizen6@gmail.com>
2020-10-01 15:19:06 +0300

    added ids

--- 6gag.ui/src/Component/post/post.jsx
+++ 6gag.ui/src/Component/post/post.jsx
@@ -4,14 +4,13 @@ import './post.css';
 
 function Post(props) {
         return(
-            <div className="post">
+            <div className="post" id={props.post.id}>
                 <div className="post-header">
                     <span className="post-title">{props.post.title}</span>
                 </div>
                 <div>
                     <Card>
                         <Card.Img src={props.post.src} alt={props.post.alt} />
-
                     </Card>
                 </div>
             </div>
